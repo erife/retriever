6f796ede91f40683d2531ddca1fb45b0d0f20c1f
erife <elaina.rife@gmail.com>
2016-09-18 15:24:33 -0700

    added state to store typed value

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
 
-const Search = () => (
-        <form>
-        <input
-    placeholder="what do you want to find?"
-    className="form-control"
-    />
-        </form>
-);
+class Search extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {term: ''}
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+    }
+
+    onInputChange(event) {
+        this.setState({ term: event.target.value });
+    }
+    
+    onFormSubmit(event) {
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+                <form onSubmit={this.onFormSubmit} className="input-group">
+                <input
+            placeholder="what do you want to find?"
+            className="form-control"
+            onChange={this.onInputChange}
+
+                />
+                <span className="input-group-btn">
+                <button type="submit" className='btn btn-secondary'>Fetch!</button>
+                </span>
+                </form>
+                
+        );
+    }
+}
+
 export default Search;
