a4bd394c31daa0e499100b96436122c157f2c8b2
erife <elaina.rife@gmail.com>
2016-09-18 15:00:34 -0700

    set up storybook, made a stub component for search box

---
+++ src/components/stories/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { storiesOf, action, linkTo } from '@kadira/storybook';
+import Button from './Button';
+import Welcome from './Welcome';
+import Search from '../Search';
+
+storiesOf('Welcome', module)
+  .add('to Storybook', () => (
+    <Welcome showApp={linkTo('Button')}/>
+  ));
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+  ))
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
+  ));
+
+storiesOf('Search', module)
+    .add('blank search', () => (
+            <Search />
+    ));
+
