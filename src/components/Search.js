6119f351a576d7da38a22017e26d1e4af9e98f74
erife <elaina.rife@gmail.com>
2016-09-18 15:08:11 -0700

    make a form box for search bar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 const Search = () => (
-        <div>searchbox</div>
+        <form>
+        <input
+    placeholder="what do you want to find?"
+    className="form-control"
+    />
+        </form>
 );
 export default Search;
---
+++ src/components/Search.js~
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Search = () => (
+        <div>searchbox</div>
+);
+export default Search;
