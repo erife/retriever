0eccc7c049b03f800c3a0c6a2441c6534e71ab93
Peter Stradinger <peter@evidation.com>
2016-09-19 13:59:37 -0700

    fixed some styles

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,79 +3,76 @@ import React, { Component } from 'react';
 var BoneImage = require('./bone.svg');
 
 const formStyle = {
-    backgroundImage: 'url(' + BoneImage + ')',
-    backgroundRepeat: "no-repeat",
-    width: "300px",
-    height: "200px",
+  backgroundImage: 'url(' + BoneImage + ')',
+  backgroundRepeat: "no-repeat",
+  width: "300px",
+  height: "200px",
   float: "left",
   padding: "0px",
-  outline: '1px dashed red'
-
 };
 const inputStyle = {
-    width: "200px",
-    marginLeft: "50px",
-    marginTop: "39px",
-    height: "50px",
-    fontSize: "16px",
+  width: "200px",
+  marginLeft: "50px",
+  marginTop: "39px",
+  height: "50px",
+  fontSize: "16px",
   border: "none",
-  outline: '1px dashed red'
 };
 const buttonDivStyle = {
-    width: "50px",
-    height: "70px",
-     float: "left",
-    paddingTop: "40px"
+  width: "50px",
+  height: "70px",
+  float: "left",
+  paddingTop: "40px"
 };
 const buttonStyle = {
-    width: "50px",
-    height: "50px",
-    backgroundColor: "#C6ED2C",
-    borderRadius: "25px",
-    color: "white",
-    textShadow: "1px 1px 1px #000000",
-    border: "1px silver solid"
+  width: "50px",
+  height: "50px",
+  backgroundColor: "#C6ED2C",
+  borderRadius: "25px",
+  color: "white",
+  textShadow: "1px 1px 1px #000000",
+  border: "1px silver solid"
 
 
 };
 
 class Search extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {term: ''}
-        this.onInputChange = this.onInputChange.bind(this);
-        this.onFormSubmit = this.onFormSubmit.bind(this);
-    }
+    this.state = {term: ''}
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
 
-    onInputChange(event) {
-        this.setState({ term: event.target.value });
-    }
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
 
-    onFormSubmit(event) {
-        event.preventDefault();
-    }
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
 
-    render() {
-        return (
-                <div>
-            <div style={formStyle}>
-            <form onSubmit={this.onFormSubmit}>
-                <input style={inputStyle}
-            placeholder="what do you want to find?"
-            onChange={this.onInputChange}
-                />
-            </form>
-            </div>
-                <div style={buttonDivStyle}>
-                <span>
-                <button style={buttonStyle} type="submit">Fetch!</button>
-                </span>
-                </div>
-                </div>
+  render() {
+    return (
+        <div>
+        <div style={formStyle}>
+        <form onSubmit={this.onFormSubmit}>
+        <input style={inputStyle}
+      placeholder="what do you want to find?"
+      onChange={this.onInputChange}
+        />
+        </form>
+        </div>
+        <div style={buttonDivStyle}>
+        <span>
+        <button style={buttonStyle} type="submit">Fetch!</button>
+        </span>
+        </div>
+        </div>
 
-        );
-    }
+    );
+  }
 }
 
 export default Search;
