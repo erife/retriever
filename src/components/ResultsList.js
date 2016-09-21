154fe2cb5f4b9034c200079d4c47701da0069d74
erife <elaina.rife@gmail.com>
2016-09-20 17:54:24 -0700

    added table to resultlist

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+const TableStyle = {
+    clear: "left"
+};
+    
 
 class ResultsList extends Component {
     render () {
         return (
-                <div>{this.props.items[0]['item']}</div>
+                <table style={TableStyle}>
+                <thead>
+                <tr>
+                <th>Item</th>
+                <th>Room</th>
+                <th>Zone</th>
+                <th>Location</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                <td>{this.props.items[0]['item']}</td>
+                <td>{this.props.items[0]['room']}</td>
+                <td>{this.props.items[0]['zone']}</td>
+                <td>{this.props.items[0]['location']}</td>
+                </tr>
+                <tr>
+                <td>{this.props.items[1]['item']}</td>
+                <td>{this.props.items[1]['room']}</td>
+                <td>{this.props.items[1]['zone']}</td>
+                <td>{this.props.items[1]['location']}</td>
+                </tr>
+                </tbody>
+                </table>
+            
         );
     }
 }
