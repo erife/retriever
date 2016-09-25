16086eccf6b6c298f8c67c185af7d9166082a0ce
erife <elaina.rife@gmail.com>
2016-09-25 11:14:03 -0700

    regex search allows for fuzzy logic

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,8 +3,10 @@ export const FETCH_ALL_ITEMS = 'FETCH_ALL_ITEMS';
 export const FETCH_FILTERED_ITEMS = 'FETCH_FILTERED_ITEMS';
     
 export function fetchFilteredItems(term) {
+    const re = new RegExp(term)
+    console.log(re)
     const full_item_list = all_items();
-    const filtered_items = full_item_list.filter((row) => { return row.item === term } );
+    const filtered_items = full_item_list.filter((row) => { return row.item.match(re) } );
     return {
         type: FETCH_FILTERED_ITEMS,
         payload: filtered_items
--- src/reducers/reducer_item_search.js
+++ src/reducers/reducer_item_search.js
@@ -1,16 +1,14 @@
-import { FETCH_ALL_ITEMS } from '../actions/index';
 import { FETCH_FILTERED_ITEMS } from '../actions/index';
 const INITIAL_STATE = {all_items: [], filtered_items: null, term: null};
 
 
 export default function(state = INITIAL_STATE, action) {
     switch(action.type) {
-    case FETCH_ALL_ITEMS:
-        return {...state, all_items: action.payload};
     case FETCH_FILTERED_ITEMS:
         return {...state, filtered_items: action.payload};
+    default:
+        return state;
     }
-    return state;
 }
 
 
